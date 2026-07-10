a32cc855ce162769ab589aeba0ad1ebdebb757c3
Nilay Kulkarni <65406074+nilaykulkarni@users.noreply.github.com>
2026-07-10 23:32:43 +0100

    Updated check_eventbrite.py

--- check_eventbrite.py
+++ check_eventbrite.py
@@ -64,8 +64,8 @@ def check_availability() -> bool:
     )
 
     # Treat it as available if it's published, not sold out, and checkout is open.
-    #return is_published and not sold_out and checkout_enabled
-    return True
+    return is_published and not sold_out and checkout_enabled
+    #return True
 
 
 def send_whatsapp(message: str) -> None:
